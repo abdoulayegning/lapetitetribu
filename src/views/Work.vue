1b82912a5df18c8eb86699612c6241e95de284c0
abdoulayegning <abdoulaye@lapetitetribu.co>
2023-03-12 00:09:39 +0000

    Update Work.vue

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,5 +1,22 @@
 <script setup>
-import { ref } from 'vue'; 
+import { ref, onBeforeMount } from 'vue'; 
+import { createClient } from 'contentful'
+
+const client = createClient({
+    space: '3dsoyu63dy91',
+    accessToken: 'fRA4jX66in_xDQqVFN8_xQNNxc36wHXnT_r9GLWaipI' //Only published !!!
+    //Preview : yaKRL3ryC8Pr9YLZiDE_tD3L5zGB6CBYxXZzgQkbaLY
+})
+
+onBeforeMount(()=>{
+    client.getEntries({
+        content_type: 'project',
+        //'fields.slug': slug,
+    })
+    .then((entries)=>{
+        //console.log(entries.items)
+    })
+})
 </script>
 
 <template>
