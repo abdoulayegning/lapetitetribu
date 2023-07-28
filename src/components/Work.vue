0b6a6e9cd62ab50fe14b772dd9434e8982772cc4
abdoulayegning <100355103+abdoulayegning@users.noreply.github.com>
2023-07-28 19:27:33 +0000

    Update

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,16 +1,21 @@
 <template>
-    <div class="container mx-auto" ref="container"> 
-        <div class="flex mt-40 mb-40 even:justify-end font-['DM_Sans'] cursor-pointer" v-for="i,j in [0,1,2,3,4,6]"> 
-            <div class="w-3/4 h-full border-red-600 relative bg-black"> 
-                <img class="opacity-50 w-full h-[50vh] object-cover" src="https://picsum.photos/2000/2000" alt=""> 
-                <div class="text-white text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <h1 class="text-5xl">hello</h1>
-                    <h2 class="font-['Benton_Italic_Regular']">hello</h2> 
-                    <!-- <h2 class="font-['PP_Neue_Machina_Plain']">hello</h2>  -->
-                </div>
-            </div> 
-        </div>  
+    <div class="bg-[#1c1c1c] pt-40 pb-40">
+        <div class="container mx-auto"> 
+            <div class="grid grid-cols-2" ref="container">
+                <div class="font-['DM_Sans'] cursor-pointer" v-for="i,j in [0,1,2,3,4,6]"> 
+                    <div class="relative h-full"> 
+                         <img class="mx-auto opacity-100 w-[75%] h-[75vh] object-cover" :src="'https://picsum.photos/id/' + j + '/2000/2000'"> 
+                        <div class="text-white text-center mt-10 mb-10">
+                            <!-- <h1 class="text-2xl">Orange Sénégal</h1> -->
+                            <h1 class="text-2xl">Demain nous appartient</h1>
+                            <h2 class="text-sm uppercase font-bold mt-2 text-white mix-blend-difference font-['PP_Neue_Machina_Plain']">Réalité Augmentée</h2> 
+                        </div> 
+                    </div> 
+                </div>  
+            </div>
+        </div>
     </div>
+    <!-- text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -->
 </template>
 
 <script setup>
@@ -27,12 +32,16 @@ onMounted(()=>{
             scaleY: 1, 
             y: 0,
             // delay: 0.45,
-            duration: 0.85,
+            duration: 1.5,
             ease: 'Expo.easeOut',
             scrollTrigger: {
                 trigger: e, 
             }
         })
+
+        if(i%2===0){
+            e.style.marginTop = 6 + 'rem'
+        }
     }
 })
 </script>
---
+++ src/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRrUW6Qq2VLN3meUbgjbquQ3tmuyvNOc0",
+  authDomain: "lapetitetribu-bae53.firebaseapp.com",
+  projectId: "lapetitetribu-bae53",
+  storageBucket: "lapetitetribu-bae53.appspot.com",
+  messagingSenderId: "111760072524",
+  appId: "1:111760072524:web:3ac18202dec9847070f9e9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
