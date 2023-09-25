b00d4b2043cf7856411b59a97869f207b0ec131c
abdoulayegning <100355103+abdoulayegning@users.noreply.github.com>
2023-09-25 20:05:48 +0000

    up

--- src/components/About.vue
+++ src/components/About.vue
@@ -30,8 +30,8 @@ onMounted(()=>{
     <div class="w-full h-[100vh] relative">  
         <div ref="roller">
             <div v-for="i in [0,1,2,3,4,6,7,8,9,10]" class="absolute whitespace-nowrap font-['DM_Sans'] text-white 
-            uppercase text-8xl mix-blend-difference top-1/2 -translate-y-1/2 pr-6 pl-6">
-                hors de <u>l'ordinaire</u>
+            uppercase text-6xl mix-blend-difference top-1/2 -translate-y-1/2 pr-6 pl-6">
+                pour des marques <span class="underline">ambitieuses</span>
             </div>
         </div> 
         <video ref="stones_video" loop muted autoplay class="w-full h-full object-cover absolute top-0 left-0 -z-10 " 
@@ -41,23 +41,28 @@ onMounted(()=>{
     <div class="container mx-auto">
         <div class="w-full lg:pt-36 pt-8 lg:pb-36 pb-8 relative"> 
             <div class="flex flex-col"> 
-                <div class="w-full font-['DM_Sans'] relative text-base font-light mt-16  p-12 rounded-3xl">   
-                    <div class="flex lg:flex-row flex-col gap-4">
+                <div class="w-full font-['DM_Sans'] relative text-base font-light mt-16  p-12 rounded-3xl">  
 
-                        <div class="text-left lg:text-6xl text-2xl lg:w-1/2 w-full flex items-center" ref="text">
-                            <div>
-                                Une expertise multidisciplinaire pour créer des expériences <span class="">uniques</span>, <span class="">interactives</span> & <span class="">engageantes</span>.
-                            </div>
-                        </div>
-
+                    <!-- <div class="flex lg:flex-row flex-col gap-4">
+ 
                         <div ref="stones_images" class="lg:block lg:w-1/2 w-full flex relative"> 
                             <img class="absolute bottom-0 left-0 lg:w-1/2 w-1" src="../assets/orange.png" alt="">
                             <img class="absolute top-0 right-0 lg:w-1/2  translate-y-0" src="../assets/pink.png" alt="">
                             <img class="absolute top-0 left-1/2 lg:w-1/2 -translate-x-1/2 translate-y-1/4" src="../assets/purple.png" alt="">
                         </div>
                         
-                    </div>
- 
+                    </div> -->
+
+                        <div class="text-left leading-loose lg:text-6xl text-xl lg:w-4/5 w-full flex items-center font-regular font-['Inter']" ref="text">
+                            <div>
+                                Une expertise multidisciplinaire pour créer des expériences <span class="">uniques</span>, <span class="">interactives</span> & <span class="">engageantes</span>.
+                            </div>
+                        </div>
+
+                        <div class="text-right leading-loose lg:text-base text-xl font-regular font-['Inter'] mt-20 font-medium uppercase">
+                                the process
+                        </div>
+
                 </div>
 
              </div>
--- src/views/Temp.vue
+++ src/views/Temp.vue
@@ -20,23 +20,23 @@ const scrollTriggerAnimations = ()=>{
     const stones_video = about.value.stones_video  
     const stones_images = about.value.stones_images  
 
-    let transforms = [
-        {angle: 50, y: '-=100'},
-        {angle: -20, y: '-=55'},
-        {angle: 15, y: '+=40'},
-    ]
+    // let transforms = [
+    //     {angle: 50, y: '-=100'},
+    //     {angle: -20, y: '-=55'},
+    //     {angle: 15, y: '+=40'},
+    // ]
 
-    for (let i = 0; i < stones_images.children.length; i++) {
-        const e = stones_images.children[i];
-        gsap.to(e, {
-            rotate: transforms[i].angle, 
-            y: transforms[i].y,
-        scrollTrigger: {
-            trigger: stones_images,
-            scrub: true,
-        }, 
-        transformOrigin:'center center'}) 
-    }
+    // for (let i = 0; i < stones_images.children.length; i++) {
+    //     const e = stones_images.children[i];
+    //     gsap.to(e, {
+    //         rotate: transforms[i].angle, 
+    //         y: transforms[i].y,
+    //     scrollTrigger: {
+    //         trigger: stones_images,
+    //         scrub: true,
+    //     }, 
+    //     transformOrigin:'center center'}) 
+    // }
 
     gsap.fromTo(stones_video, {scale: 0.45}, {scale: 0.85, ease: 'Expo.easeOut', scrollTrigger: {
         trigger: stones_video,
