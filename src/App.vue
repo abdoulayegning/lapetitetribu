1bc5b2a776c6d008b879e6dc074e31d7a12589c1
abdoulayegning <100355103+abdoulayegning@users.noreply.github.com>
2024-02-06 10:58:58 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>  
 import Lenis from '@studio-freight/lenis'
-import {onMounted} from 'vue'
+import { onMounted, ref } from 'vue'
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import Header from './components/Header.vue';
+
+const contactBtn = ref(null)
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -17,11 +19,23 @@ onMounted(()=>{
     })
 
     gsap.ticker.lagSmoothing(0)
+
+    //Contact button
+    const redDot = contactBtn.value.children[0]
+    gsap.fromTo(redDot, {opacity: 0}, {opacity: 1, repeat: -1})
 })
 
 </script>
 
-<template> 
+<template>
+  
+  <div ref="contactBtn" 
+  class="fixed z-[150] bottom-7 right-7 bg-black text-white hover:bg-white hover:text-black hover:border hover:border-black
+  rounded-full pr-6 pl-6 pt-2 pb-2 uppercase text-xs flex gap-2 justify-center items-center cursor-pointer">
+    <div class="w-2 h-2 bg-red-600 rounded-full"></div>
+    <div>Vous avez un projet ?</div>
+  </div>
+
   <Header></Header>  
   <router-view></router-view> 
 </template>
