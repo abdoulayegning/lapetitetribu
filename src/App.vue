0b23af05a5a7cf2bceb99264399ad53b48e12a3b
abdoulayegning <100355103+abdoulayegning@users.noreply.github.com>
2024-02-07 09:01:48 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,12 @@ onMounted(()=>{
     gsap.ticker.lagSmoothing(0)
 
     //Contact button
+    contactBtn.value.addEventListener("click", ()=>{
+      window.open('mailto:abdoulaye@lapetitetribu.co');
+    })
     const redDot = contactBtn.value.children[0]
     gsap.fromTo(redDot, {opacity: 0}, {opacity: 1, repeat: -1})
+    gsap.fromTo(contactBtn.value, {y: 0}, {y: 10, repeat: -1, yoyo: true, ease: 'power4.inOut'})
 })
 
 </script>
@@ -30,10 +34,10 @@ onMounted(()=>{
 <template>
   
   <div ref="contactBtn" 
-  class="fixed z-[150] bottom-7 right-7 bg-black text-white hover:bg-white hover:text-black hover:border hover:border-black
-  rounded-full pr-6 pl-6 pt-2 pb-2 uppercase text-xs flex gap-2 justify-center items-center cursor-pointer">
-    <div class="w-2 h-2 bg-red-600 rounded-full"></div>
-    <div>Vous avez un projet ?</div>
+  class="fixed z-[150] bottom-7 left-1/2 -translate-x-1/2 bg-red-600 text-white hover:bg-white hover:text-black hover:border hover:border-black
+  rounded-full pr-6 pl-6 pt-3 pb-3 uppercase text-xs flex gap-2 justify-center items-center cursor-pointer">
+    <div class="w-2 h-2 bg-black rounded-full"></div>
+    <div class="leading-none">Vous avez un projet ?</div>
   </div>
 
   <Header></Header>  
