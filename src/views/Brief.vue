d8f271138dd50c43d93f71519b46ab5192a5784e
abdoulayegning <100355103+abdoulayegning@users.noreply.github.com>
2024-02-15 10:20:16 +0000

    update

--- src/views/Brief.vue
+++ src/views/Brief.vue
@@ -1,78 +1,79 @@
-<template>
-
-<div class="w-full pb-40 pt-20 mx-auto container">
-
-    <div class="font-['DM_Sans']"> 
-
-        <div class="flex gap-8">
-            <div class="w-1/2">
-                <div class="">
-                    <span>Quel type de projet ?</span>
-                    <select name="" id="">
-                        <option value="">Branding & Web</option>
-                        <option value="">Gaming & VR, AR</option>
-                    </select>
-                </div>
-        
-                <div> 
-                    <span>Quel est votre budget ?</span>
-                    <select name="" id="">
-                        <option value="">500K</option>
-                        <option value="">800K</option>
-                        <option value="">1 500 000 +</option>
-                    </select> 
-                </div>
-        
-                <div>
-                    <span>A qui ai je l'honneur ?</span>
-                    <input type="text" placeholder="Prénom(s) & Nom" id="">
-                </div>
-        
-                <div>
-                    <span>Avez vous un email ?</span>
-                    <input type="email" placeholder="Email" id="">
-                </div>
-            </div>
-    
-            <div class="w-1/2">
-                <div>
-                    <span>Dans quelle entreprise travaillez-vous ? </span>
-                    <input type="text" placeholder="Votre entreprise" id="">
-                </div>
-                
-                <div> 
-                    <span>Quel votre rôle dans l'entreprise ?</span>
-                    <select name="" id="">
-                        <option value="">Chef d'agence</option> 
-                        <option value="">Chef Marketing</option> 
-                        <option value="">Digital Manager</option> 
-                        <option value="">Graphiste</option> 
-                        <option value="">Web Designer</option> 
-                        <option value="">Développeur</option> 
-                    </select> 
-                </div>
-        
-                <div> 
-                    <span>Quel est l'effectif de votre entreprise ?</span>
-                    <select name="" id="">
-                        <option value="">1 à 10</option> 
-                        <option value="">10 à 30</option> 
-                        <option value="">30+</option> 
-                    </select> 
-                </div>
-            </div>
+<template> 
+    <div class="w-full pb-40 pt-32 mx-auto container">
+        {{ entry }}
+        <div class="uppercase mb-2 grid lg:grid-cols-2 gap-3">
+            <input v-model="name" placeholder="prénom(s) & nom" class="bg-transparent border-black border-b uppercase text-center" type="text" name="" id="">
+            <input v-model="company" placeholder="Votre entreprise" class="bg-transparent border-black border-b uppercase text-center" type="text" name="" id="">
+            <input v-model="email" placeholder="Email" class="bg-transparent border-black border-b uppercase text-center" type="text" name="" id="">
+            <input v-model="phone" placeholder="Téléphone" class="bg-transparent border-black border-b uppercase text-center" type="text" name="" id="">
+        </div> 
+        <div class="uppercase mb-3 text-center mt-10 font-semibold">Nombre d'employés dans votre entreprise</div>
+        <div class="grid lg:grid-cols-3 gap-3 uppercase justify-center" ref="companySize">
+             <div :data-company-size="size" class="btn" v-for="size in sizes">{{ size }}</div>
+          </div>
+        <!-- Projet -->
+        <div class="uppercase mb-3 mt-10 text-center font-bold">sur quel type de projet voulez vous collaborer avec nous ?</div>
+        <div class="grid lg:grid-cols-3 gap-3 uppercase justify-center" ref="services">
+            <div class="btn">un site e-commerce</div>
+            <div class="btn">une landing page</div>
+            <div class="btn">un mini-jeu fun</div>
+            <div class="btn">un site portfolio</div>
+            <div class="btn">une vidéo animée</div>
+            <div class="btn">faire mon branding</div>
         </div>
 
-        
- 
-        <div>
-            <textarea placeholder="Décrivez nous votre projet ?" id="" cols="30" rows="10"></textarea>
-        </div>
 
-    </div> 
-
-</div>
+    </div>
 </template>
+
+<style scoped>
+.btn {
+    @apply pt-2 pb-2 pr-12 pl-12 border rounded-full text-center cursor-pointer hover:bg-black hover:text-white
+}
+</style>
+
+<script setup> 
+import { onMounted, ref } from 'vue';
+
+const services = ref(null)
+const companySize = ref(null)
+const sizes = ['1-10', '10-30', "+30"]
+const name = ref('')
+const company = ref('')
+const email = ref('')
+const phone = ref('')
+
+let entry = ref(
+    {
+        name: name, 
+        company: company,
+        email: email,
+        phone: phone,
+        service: '',
+        companySize: ''
+    },
+)
+ 
+onMounted(()=>{ 
+
+    for (let i = 0; i < services.value.children.length; i++) {
+        const element = services.value.children[i];
+        element.addEventListener('click', (e)=>{
+            let selected = e.target.innerHTML
+            entry.value.service = selected
+        })
+    }
+
+    for (let i = 0; i < companySize.value.children.length; i++) {
+        const element = companySize.value.children[i];
+        element.addEventListener('click', (e)=>{
+            let selected = e.target.innerHTML
+            entry.value.companySize = selected
+        })
+    }
+})
+
+</script>
 
 <style scoped>
 select, input, textarea {
