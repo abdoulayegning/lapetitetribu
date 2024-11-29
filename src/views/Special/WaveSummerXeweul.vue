1c03fcd9fcba03b108b951da425c5eac97e55df1
abdoulayegning <100355103+abdoulayegning@users.noreply.github.com>
2024-11-29 12:40:44 +0000

    Update WaveSummerXeweul.vue

--- src/views/Special/WaveSummerXeweul.vue
+++ src/views/Special/WaveSummerXeweul.vue
@@ -27,7 +27,10 @@
         
          
         
-        <div class="mt-24"></div>
+        <div class="mt-24">
+            <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1034507359?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="La Petite Tribu x Wave Sénégal - Summer Xeweul 2024 - Case Study"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+        </div>
+
         <div class="text-3xl text-center font-['DM_Sans'] font-bold">Mécaniques inspirées d’un classique</div>
         <div class="mt-8 mb-24 text-xl text-center font-['DM_Sans'] font-medium">
             Garder les mécaniques de Tetris sur la logique de la construction tout en faisant le rappel du nom de la marque.
