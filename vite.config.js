2cb75d992803667ec6f96ed2b61e73e2af66cfa3
abdoulayegning <100355103+abdoulayegning@users.noreply.github.com>
2025-07-20 12:27:20 +0000

    Update project dependencies and config
    Upgraded major dependencies including Vite, esbuild, and @vitejs/plugin-vue to latest versions. Added vite-plugin-image-optimizer and vite-plugin-sitemap. Renamed project in package.json. Added robots.txt to public directory and updated vite.config.js for new plugins.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
+import { sitemap } from 'vite-plugin-sitemap'
+import { imageOptimizer } from 'vite-plugin-image-optimizer'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+
+    vue(),
+
+    imageOptimizer({
+      jpg: { quality: 80 },
+      png: { quality: 80 },
+      webp: { quality: 80 },
+    }),
+
+    sitemap({
+      hostname: 'https://lapetitetribu.co',
+      dynamicRoutes: ['/[slug]'] // For dynamic routes
+    })
+
+  ]
 })
